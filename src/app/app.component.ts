a055e2ba84dc7a9709787ce1b107f83030aafd15
Abdul Ahad <abdul.ahad@techlogix.com>
2022-12-08 12:15:08 +0500

    products card component completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -84,6 +84,29 @@ export class AppComponent implements OnInit {
       discountedPrice: '8,000',
       imgSrc: '../assets/product.jpg',
     },
+    {
+      title: 'MISSHA',
+      description:
+        'MISSHA All-around Safe Block Soft Finish Sun Milk SPF50+ PA+++ 70ml',
+      price: '22,000',
+      discountedPrice: '18,000',
+      imgSrc: '../assets/product_1.jpg',
+    },
+    {
+      title: 'COSRX',
+      description: 'COSRX ADVANCED SNAIL 92 ALL IN ONE CREAM 100ml',
+      price: '10,000',
+      discountedPrice: '8,000',
+      imgSrc: '../assets/product.jpg',
+    },
+    {
+      title: 'MISSHA',
+      description:
+        'MISSHA All-around Safe Block Soft Finish Sun Milk SPF50+ PA+++ 70ml',
+      price: '22,000',
+      discountedPrice: '18,000',
+      imgSrc: '../assets/product_1.jpg',
+    },
   ];
   constructor(private primengConfig: PrimeNGConfig) {}
 
