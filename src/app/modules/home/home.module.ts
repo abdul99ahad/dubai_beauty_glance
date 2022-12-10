5eb8263e5b33ee0b5e8522ed294eda68a3b95018
Abdul Ahad <abdul.ahad@techlogix.com>
2022-12-10 21:33:43 +0500

    added homepage into separate page via routing

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainTimedealComponent } from './main-timedeal/main-timedeal.component';
 import { SharedModule } from '../shared/shared.module';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [MainTimedealComponent],
-  imports: [CommonModule, SharedModule],
+  declarations: [MainTimedealComponent, HomepageComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule.forRoot([{ path: 'home', component: HomepageComponent }]),
+  ],
   exports: [MainTimedealComponent],
 })
 export class HomeModule {}
