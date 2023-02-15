b5239a457551c8d7d57933b8afe685d1c7933a37
Muhammad Waqar <muhammadwaqar@folio3.com>
2023-02-15 22:07:22 +0500

    Categories popup splitted to 5 columns

--- src/app/modules/shared/components/header/header.component.ts
+++ src/app/modules/shared/components/header/header.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem } from 'primeng/api';
-import { HttpClient } from '@angular/common/http';
-import {
-  debounce,
-  interval,
-  map,
-  Subject,
-  Subscription,
-  switchMap,
-} from 'rxjs';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { MenuItem } from "primeng/api";
+import { HttpClient } from "@angular/common/http";
+import { map, Subject, Subscription, switchMap, } from "rxjs";
+import { Router } from "@angular/router";
 
 interface Product {
   name: string;
@@ -20,62 +13,80 @@ interface Product {
 }
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit {
   expandedState: boolean = false;
   mobileDisplay: boolean = false;
   navBarItemList: any = [
-    { page: 'NEW', url: 'products' },
-    { page: 'BEST', url: 'products' },
-    { page: 'TIME DEAL', url: 'products' },
-    { page: 'PROMOTIONS', url: 'promotions' },
-    { page: 'BRAND', url: 'brands' },
-    { page: 'COUPON', url: 'products' },
+    {
+      page: "NEW",
+      url: "products"
+    },
+    {
+      page: "BEST",
+      url: "products"
+    },
+    {
+      page: "TIME DEAL",
+      url: "products"
+    },
+    {
+      page: "PROMOTIONS",
+      url: "promotions"
+    },
+    {
+      page: "BRAND",
+      url: "brands"
+    },
+    {
+      page: "COUPON",
+      url: "products"
+    },
   ];
 
   navBarItemListMobile: any = [
     {
-      page: 'NEW',
-      url: 'products',
+      page: "NEW",
+      url: "products",
     },
     {
-      page: 'BEST',
-      url: 'products',
+      page: "BEST",
+      url: "products",
     },
     {
-      page: 'TIME DEAL',
-      url: 'products',
+      page: "TIME DEAL",
+      url: "products",
     },
     {
-      page: 'PROMOTIONS',
-      url: 'promotions',
+      page: "PROMOTIONS",
+      url: "promotions",
     },
   ];
   recentlyViewedItems: MenuItem[];
   display: boolean = false;
 
   public productSearchInputStyles = {
-    width: '100%',
-    'border-top-style': 'hidden',
-    'border-right-style': 'hidden',
-    'border-left-style': 'hidden',
-    'border-bottom-style': 'groove',
-    'border-bottom-width': 'thick',
-    'border-bottom-color': 'black',
-    'min-width': '90%',
-    height: '100%',
+    width: "100%",
+    "border-top-style": "hidden",
+    "border-right-style": "hidden",
+    "border-left-style": "hidden",
+    "border-bottom-style": "groove",
+    "border-bottom-width": "thick",
+    "border-bottom-color": "black",
+    "min-width": "90%",
+    height: "100%",
   };
 
   public productSearchMobileViewInputStyles = {
-    width: '100%',
-    height: '35px',
-    padding: '0 0 0 20px',
-    border: '0',
-    'border-radius': '30px',
-    background: '#ededed',
+    width: "100%",
+    height: "35px",
+    padding: "0 0 0 20px",
+    border: "0",
+    "border-radius": "30px",
+    background: "#ededed",
   };
 
   public products: Array<Product> = [];
@@ -85,7 +96,8 @@ export class HeaderComponent implements OnInit {
   public constructor(
     private readonly http: HttpClient,
     private readonly router: Router
-  ) {}
+  ) {
+  }
 
   public navBarMobileListViewToggle(): void {
     if (this.expandedState) {
@@ -99,16 +111,16 @@ export class HeaderComponent implements OnInit {
   public ngOnInit(): void {
     this.recentlyViewedItems = [
       {
-        label: 'Item 1',
-        icon: 'pi pi-fw pi-plus',
+        label: "Item 1",
+        icon: "pi pi-fw pi-plus",
       },
       {
-        label: 'Item 2',
-        icon: 'pi pi-fw pi-download',
+        label: "Item 2",
+        icon: "pi pi-fw pi-download",
       },
       {
-        label: 'Item 3',
-        icon: 'pi pi-fw pi-refresh',
+        label: "Item 3",
+        icon: "pi pi-fw pi-refresh",
       },
     ];
 
@@ -119,25 +131,27 @@ export class HeaderComponent implements OnInit {
     this.searchProductDebouncedSubject.next(searchString);
   }
 
-  private setupColdSubscriberForLiveProductSearching() {
-    this.coldSubscriber = this.searchProductDebouncedSubject
-      .pipe(
-        debounce(() => interval(1000)),
-        switchMap((searchText: string) =>
-          this.http.get<{ data: Array<Product> }>(
-            `https://api.technosavvyllc.com/beautymallkorea/public/api/products?paginate=0&productName=${searchText}`
-          )
-        ),
-        map(({ data }) => data)
-      )
-      .subscribe((products: Array<Product>) => (this.products = products));
-  }
-
   public toggleHamBurgerMenu(): void {
     this.display = !this.display;
   }
 
   public navigateToProduct(product: Product): void {
-    this.router.navigate(['/product', product.slug]);
+    this.router.navigate([
+      "/product",
+      product.slug
+    ]);
+  }
+
+  private setupColdSubscriberForLiveProductSearching() {
+    this.coldSubscriber = this.searchProductDebouncedSubject
+      .pipe(
+        switchMap((searchText: string) =>
+          this.http.get<{ data: Array<Product> }>(
+            `https://api.technosavvyllc.com/beautymallkorea/public/api/products?paginate=0&productName=${ searchText }`
+          )
+        ),
+        map(({ data }) => data)
+      )
+      .subscribe((products: Array<Product>) => (this.products = products));
   }
 }
