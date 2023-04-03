197cc4a3c046b57ad70cbf1c0b795abc4aca3a13
Abdul Ahad <abdul.ahad@techlogix.com>
2023-04-04 00:04:56 +0500

    product can be added to cart from card

--- src/app/modules/shared/product-card/product-card.component.ts
+++ src/app/modules/shared/product-card/product-card.component.ts
@@ -1,4 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { CartService } from 'ng-shopping-cart';
+import { map, Observable } from 'rxjs';
+import { ProductDetail } from 'src/app/interfaces/product.interface';
+import { WebApiService } from 'src/app/services/web-api.service';
+import { ProductCartItem } from 'src/app/utilities/productCartItem';
 import { CurrencyService } from '../../../services/currency.service';
 
 @Component({
@@ -15,8 +20,42 @@ export class ProductCardComponent {
   @Input('brandName') brandName: string;
 
   public readonly currency: string;
+  public productDetail: ProductDetail;
 
-  public constructor(private readonly currencyService: CurrencyService) {
+  public constructor(
+    private readonly currencyService: CurrencyService,
+    private cartService: CartService<ProductCartItem>,
+    private webApiService: WebApiService
+  ) {
     this.currency = this.currencyService.selectedCurrency;
+  }
+
+  public getProductDetail(): Observable<ProductDetail> {
+    return this.webApiService
+      .getProductDetails(this.slug)
+      .pipe(map((response: { data: ProductDetail }) => response.data));
+  }
+
+  public AddtoCart() {
+    // If already exists, increase the quantity
+    this.getProductDetail().subscribe((detail) => {
+      // this.productDetail = detail;
+      if (this.cartService.getItem(detail.sku)) {
+        const product = this.cartService.getItem(detail.sku);
+        product.setQuantity(product.quantity + detail.min_order_quantity);
+      } else {
+        this.cartService.addItem(
+          new ProductCartItem({
+            id: detail.sku,
+            name: detail.name,
+            image: this.image,
+            price: this.price,
+            quantity: detail.min_order_quantity,
+            discount_price: this.discount_price,
+            min_quantity: detail.min_order_quantity,
+          })
+        );
+      }
+    });
   }
 }
