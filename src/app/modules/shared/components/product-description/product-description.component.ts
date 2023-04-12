f9725a056c2133bec5c0fb5e13bbe64461dd1cd4
Abdul Ahad <abdul.ahad@techlogix.com>
2023-04-12 23:27:22 +0500

    product detail side view dynamic

--- src/app/modules/shared/components/product-description/product-description.component.ts
+++ src/app/modules/shared/components/product-description/product-description.component.ts
@@ -1,7 +1,15 @@
 import { Component, HostListener, Input, OnInit } from '@angular/core';
-import { ProductDetail } from 'src/app/interfaces/product.interface';
-import {CurrencyService} from "../../../../services/currency.service";
-
+import { CartService } from 'ng-shopping-cart';
+import {
+  Price,
+  Product,
+  ProductDetail,
+  ProductOptions,
+  ProductVariantList,
+} from 'src/app/interfaces/product.interface';
+import { ProductCartItem } from 'src/app/utilities/productCartItem';
+import { CurrencyService } from '../../../../services/currency.service';
+type SelectedProductOption = { [productOptionIndex: number]: boolean };
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
@@ -13,6 +21,7 @@ export class ProductDescriptionComponent implements OnInit {
 
   @Input() totalPrice: number = 0;
   @Input() productDetail: ProductDetail;
+  @Input() productVariants: ProductVariantList;
   country: string = 'PK';
   checked: boolean = true;
   display: boolean = false;
@@ -36,11 +45,22 @@ export class ProductDescriptionComponent implements OnInit {
   floatDiscountedPrice: string;
   public readonly currency: string;
 
-  constructor(private readonly currencyService: CurrencyService) {
+  constructor(
+    private readonly currencyService: CurrencyService,
+    private cartService: CartService<ProductCartItem>
+  ) {
     this.currency = this.currencyService.selectedCurrency;
   }
 
   ngOnInit(): void {
+    console.log(this.productDetail);
+    this.selectedQuantity = this.productDetail.min_order_quantity;
+    this.updateBasePrice(
+      this.productDetail.price,
+      this.productDetail.discount_price
+    );
+    this.updateTotalPrice(this.selectedQuantity);
+    this.manipulateProductOptionsJson();
     this.price = parseFloat(this.productDetail.price);
     this.discountedPrice = parseFloat(
       this.productDetail.discount_price
@@ -50,12 +70,6 @@ export class ProductDescriptionComponent implements OnInit {
 
     this.floatPrice = this.price.toPrecision(4);
     this.floatDiscountedPrice = this.discountedPrice.toPrecision(4);
-  }
-
-  @HostListener('mousewheel', ['$event'])
-  onMousewheel(event: any) {
-    if (this.display) return;
-    this.display = true;
   }
 
   checkSkuItemTrue(item: any): void {
@@ -76,15 +90,184 @@ export class ProductDescriptionComponent implements OnInit {
     );
   }
 
+  public discountedTotalPrice = 0.0;
+
+  cartDisplay: boolean = false;
+  selectedIndex: number;
+  variantSelectedIndex: number = -1;
+
+  public checkedOptions: SelectedProductOption = {};
+
+  public productBasePrice: Price;
+
+  response: boolean = false;
+
+  ItemAddedToCartProduct: { name: string; image: string } = {
+    name: '',
+    image: '',
+  };
+
+  productVariantName: string;
+  slug: string;
+
+  private previousElement: HTMLElement;
+
+  public AddtoCart(productDetail: ProductDetail) {
+    this.cartDisplay = true;
+    this.ItemAddedToCartProduct = {
+      name: productDetail.name,
+      image: productDetail.image,
+    };
+    // If already exists, increase the quantity
+    if (
+      this.cartService.getItem(this.productDetail.sku + this.productVariantName)
+    ) {
+      const product = this.cartService.getItem(
+        this.productDetail.sku + this.productVariantName
+      );
+      product.setQuantity(product.quantity + this.selectedQuantity);
+    } else {
+      this.cartService.addItem(
+        new ProductCartItem({
+          id: this.productDetail.sku + this.productVariantName,
+          name: this.productDetail.name,
+          image: this.productDetail.image,
+          price: this.productDetail.price,
+          quantity: this.selectedQuantity,
+          discount_price: this.productDetail.discount_price,
+          min_quantity: this.productDetail.min_order_quantity,
+          slug: this.slug,
+        })
+      );
+    }
+  }
+
+  public updateBasePrice(price: string, discounted_price: string | null) {
+    this.productBasePrice = {
+      price,
+      discounted_price,
+    };
+  }
+
   public quantityUp(): void {
-    // this.selectedQuantity++;
-    // this.updateTotalPrice(this.selectedQuantity);
+    this.selectedQuantity++;
+    this.updateTotalPrice(this.selectedQuantity);
   }
 
   public quantityDown(): void {
-    // if (this.selectedQuantity > this.productDetail.min_order_quantity) {
-    //   this.selectedQuantity--;
-    //   this.updateTotalPrice(this.selectedQuantity);
-    // }
+    if (this.selectedQuantity > this.productDetail.min_order_quantity) {
+      this.selectedQuantity--;
+      this.updateTotalPrice(this.selectedQuantity);
+    }
   }
+
+  public toggleProductOptionSelection(productOption: ProductOptions): void {
+    const productOptionIndex: number =
+      this.productDetail.productOptions.findIndex((x) => x == productOption);
+    // if (this.checkedOptions[productOptionIndex]) {
+    //   this.checkedOptions[productOptionIndex] =
+    //     !this.checkedOptions[productOptionIndex];
+    //   return;
+    // }
+    // this.variantSelectedIndex = productOptionIndex;
+
+    if (productOption.price_adjustment == 1) {
+      // Add to base price
+      this.productDetail.price = (
+        parseFloat(this.productBasePrice.price) +
+        parseFloat(productOption.price_difference)
+      ).toString();
+      if (this.productBasePrice.discounted_price) {
+        this.productDetail.discount_price = (
+          parseFloat(this.productBasePrice.discounted_price) +
+          parseFloat(productOption.price_difference)
+        ).toString();
+      }
+    } else {
+      this.productDetail.price = (
+        parseFloat(this.productBasePrice.price) -
+        parseFloat(productOption.price_difference)
+      ).toString();
+      if (this.productBasePrice.discounted_price) {
+        this.productDetail.discount_price = (
+          parseFloat(this.productBasePrice.discounted_price) -
+          parseFloat(productOption.price_difference)
+        ).toString();
+      }
+    }
+
+    this.updateTotalPrice(this.selectedQuantity);
+    this.productVariantName = productOption.optionValue.name;
+    this.checkedOptions[productOptionIndex] = true;
+    if (productOption.optionValue.image)
+      this.changeMainImage(productOption.optionValue.image, productOptionIndex);
+  }
+
+  private manipulateProductOptionsJson() {
+    this.productVariants = this.groupBy(
+      this.productDetail.productOptions,
+      'optionValue.option.name'
+    );
+  }
+
+  public changeMainImage(source: string, index: number): void {
+    this.productDetail.image = source;
+    this.variantSelectedIndex = index;
+  }
+
+  public updateCss(event: Event): void {
+    const clickedElement = event.target as HTMLElement;
+    // If a previous element exists, revert its CSS back to the original state
+    if (this.previousElement) {
+      this.previousElement.style.border = 'none';
+    }
+    clickedElement.style.border = '1px solid red';
+
+    this.previousElement = clickedElement;
+  }
+  public updateTotalPrice(quantity: number): void {
+    if (quantity <= 0) return;
+
+    const productPrice = this.productDetail.discount_price
+      ? +this.productDetail.discount_price
+      : +this.productDetail.price;
+
+    this.totalPrice = parseFloat(
+      (+this.productDetail.price * quantity).toFixed(2)
+    );
+    this.discountedTotalPrice = parseFloat(
+      (productPrice * quantity).toFixed(2)
+    );
+  }
+
+  settings = {
+    counter: false,
+    // plugins: [lgZoom],
+  };
+
+  private groupBy = (arrayToGroup: any, byKey: string) => {
+    const groupedMap: any = {};
+
+    const props = byKey.split('.');
+    for (let item of arrayToGroup) {
+      const valueToGroupBy = props.reduce((reducedItem, currentProp) => {
+        return reducedItem[currentProp];
+      }, item);
+
+      const groupedItemsByCurrentValue = groupedMap[valueToGroupBy];
+      if (groupedItemsByCurrentValue) {
+        groupedItemsByCurrentValue.push(item);
+
+        continue;
+      }
+
+      groupedMap[valueToGroupBy] = [item];
+    }
+
+    return groupedMap;
+  };
+  // onBeforeSlide = (detail: BeforeSlideDetail): void => {
+  //   const { index, prevIndex } = detail;
+  //   console.log(index, prevIndex);
+  // };
 }
